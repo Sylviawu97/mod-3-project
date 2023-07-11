bbb8b4cfc5eca8dfc12a5a5d86a5b85e520fbae6
Meixuan Wu <mwu@LABA.PSWAS.LOCAL>
2023-07-11 15:46:01 -0700

    pages

--- src/components/CareerAdvice.js
+++ src/components/CareerAdvice.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./CareerAdvice.css"
+import Card from './SC-card'
 
 const CareerAdvice = () => {
     return (
         <div className="sec__one">
-            <h1> Career Advice </h1>
+        <div className="card-cover">
+            <div className="col-md-12">
+                <div className="row">
+                    <div className="col-md-4 mb-2">
+                        <Card title="Mock Interview" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSfXadxcInsMdj46ssnFR6Nt77nP6LZyI3AQ&usqp=CAU" text="Our mock interview service helps individuals:
+
+Practice and refine their interview skills: By participating in mock interviews, individuals can practice answering common interview questions, learn effective techniques for structuring their responses, and refine their overall interview performance.
+Gain confidence: Mock interviews provide a safe environment to practice and build confidence in handling challenging interview situations, reducing anxiety and boosting self-assurance.
+Receive personalized feedback: Our experts provide valuable feedback on areas that need improvement, highlighting strengths and suggesting strategies to overcome weaknesses.
+Learn industry-specific interview techniques: Our professionals have industry knowledge and can guide candidates on industry-specific interview techniques, ensuring they are well-prepared for job interviews in their desired field."/>
+                   
+                   </div>
+                    <div className="col-md-4 mb-2">
+                    <Card title="Mentorship" img="https://www.cis-spain.com/en/wp-content/uploads/sites/3/2021/06/mentoring-program-CIS-university.jpeg" text="At our hackathon, we invite you to unleash your creativity. It's your chance to explore uncharted territories, think outside the box, and bring your wildest ideas to life.Collaborate with like-minded peers and gain new perspectives as you work together to solve real-world problems." />
+                    </div>
+                
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
--- src/components/SummerCamp.js
+++ src/components/SummerCamp.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import Card from './SC-card';
+import Card from './SC-card'
 import './SummerCamp.css'
 
 const SummerCamp= () => {
     return (
         <div className="sec__one">
-             <h2 className="main-title text-center">Fun SummerCamp for Teens</h2>
         <div className="card-cover">
             <div className="col-md-12">
                 <div className="row">
                     <div className="col-md-4 mb-2">
-                        <Card title="Tech Mini Camp" img="" />
+                        <Card title="Introduction to Python Programming and Machine Learning" img="https://thumbs.dreamstime.com/b/tech-camp-children-vr-technologies-design-educational-course-learning-programming-engineering-meta-universe-summer-279457070.jpg" text="   Machine Learning, Data Science, and the use of Artificial Intelligence technologies is growing rapidly in our society. Just a few applications include self-driving cars, personal assistants, product recommendations, robotics, data analysis, and web searching. These applications typically involve self-learning systems that are trained based on large amounts of data and the integration of “intelligence” based on algorithms. In this course, students will be introduced to programming in Python, the language of Machine Learning, and to one of the most common applications in Machine Learning, the image classifier. Students will create a basic image classifier using the Google Teachable Machine tool and integrate it into an application framework, both in a web page, and on a Raspberry Pi platform." />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="E-Commerce Services" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                    <Card title="An exciting teen hackathon" img="https://www.shutterstock.com/image-vector/tech-camp-children-computer-science-260nw-2306476999.jpg" text="At our hackathon, we invite you to unleash your creativity. It's your chance to explore uncharted territories, think outside the box, and bring your wildest ideas to life.Collaborate with like-minded peers and gain new perspectives as you work together to solve real-world problems." />
                     </div>
-                    <div className="col-md-4 mb-2">
-                    <Card title="Cyber Security" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
-                    </div>
+                
                 </div>
             </div>
         </div>

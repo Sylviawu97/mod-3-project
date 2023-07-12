25bce70df696c169fa8b351597e9939be12ee0e5
Meixuan Wu <mwu@LABA.PSWAS.LOCAL>
2023-07-12 14:30:24 -0700

    3

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-
-
+const express = require('express');
+const path = require('path');
 const ApplicationForm = require("./models/Application");
 const express = require('express');
 const dotenv = require('dotenv');
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/build")));
 dotenv.config();
 require("./config/database")
 // lets us do process.env (get variables from .env file)
 // now I can use process.env.VARIABLE_NAME
 // when my server starts, I want to connect to my database
+
+
 
 const app = express();
 app.use(express.json());
